004f63aaf4ce996b77499726964f27632b142c40
Joaquin Perez <perezmoralesJ30@gmail.com>
2026-01-31 12:13:16 +0100

    Docs: Añadida documentación Godoc y enlace externo

--- calculator.go
+++ calculator.go
@@ -1,6 +1,12 @@
-package calculator
+// Package calculadora proporciona herramientas sencillas para realizar operaciones
+// aritméticas básicas como parte de un ejemplo de aprendizaje de módulos.
+package calculadora
 
-// Add recibe dos enteros y devuelve su suma.
+// Add devuelve la suma de dos números enteros.
+//
+// Es una implementación básica de la operación aritmética de adición.
+// Para más información sobre los fundamentos de la suma, consulta:
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
